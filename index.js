35ebda0caf52ac770055e997ad685cac1aed22a5
Natalia Sofia <nataliasofiamatos@gmail.com>
2021-11-26 16:18:46 -0500

    cleanup with eslint

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
-let state = {
-  gameStatus: "Unstarted",
-  gameClock: "12:00",
+const state = {
+  gameStatus: 'Unstarted',
+  gameClock: '12:00',
   gameClockTimer: null,
-  shotClock: "40",
+  shotClock: '40',
   shotClockTimer: null,
-  gameQuarter: "1st",
+  gameQuarter: '1st',
   homeScore: 0,
   visitorScore: 0,
   timeoutHome: 7,
   timeoutVisitor: 7,
-  homeRank: 2, 
+  homeRank: 2,
   visitorRank: 4,
-  homeTeam: "PHX",
-  visitorTeam: "LAC"
-}
-window.onload = render
+  homeTeam: 'PHX',
+  visitorTeam: 'LAC',
+};
+window.onload = render;
--- renderers.js
+++ renderers.js
@@ -1,80 +1,80 @@
 const render = () => {
-  renderGameClock()
-  renderScores()
-  renderTimeOuts()
-  renderShotClock()
-  renderPlayButton()
-  renderScoreButtons()
-  renderTeamNames()
-  renderRanks()
-}
+  renderGameClock();
+  renderScores();
+  renderTimeOuts();
+  renderShotClock();
+  renderPlayButton();
+  renderScoreButtons();
+  renderTeamNames();
+  renderRanks();
+};
 const renderGameClock = () => {
-  let gameClock = document.getElementsByClassName("top-of-time") [0]
-  let gameQuarter = document.getElementsByClassName("game-quarter") [0]
-  gameClock.textContent = state.gameClock
-  gameQuarter.textContent = state.gameQuarter
-}
+  const gameClock = document.getElementsByClassName('top-of-time')[0];
+  const gameQuarter = document.getElementsByClassName('game-quarter')[0];
+  gameClock.textContent = state.gameClock;
+  gameQuarter.textContent = state.gameQuarter;
+};
 const renderScores = () => {
-  let homeScore = document.getElementsByClassName("home-score")[0]
-  let visitorScore = document.getElementsByClassName("visitor-score")[0]
-  homeScore.textContent = state.homeScore
-  visitorScore.textContent = state.visitorScore
-}
+  const homeScore = document.getElementsByClassName('home-score')[0];
+  const visitorScore = document.getElementsByClassName('visitor-score')[0];
+  homeScore.textContent = state.homeScore;
+  visitorScore.textContent = state.visitorScore;
+};
 const renderTimeOuts = () => {
-  let timeoutHome = document.getElementsByClassName("timeout-home")[0]
-  let timeoutVisitor = document.getElementsByClassName("timeout-visitor")[0]
-  timeoutHome.textContent = "Timeout:" + state.timeoutHome
-  timeoutVisitor.textContent = "Timeout:" + state.timeoutVisitor
-}
+  const timeoutHome = document.getElementsByClassName('timeout-home')[0];
+  const timeoutVisitor = document.getElementsByClassName('timeout-visitor')[0];
+  timeoutHome.textContent = `Timeout:${state.timeoutHome}`;
+  timeoutVisitor.textContent = `Timeout:${state.timeoutVisitor}`;
+};
 const renderShotClock = () => {
-  let shotClock = document.getElementsByClassName("shot-clock") [0]
-  shotClock.textContent = state.shotClock
-  shotClock.style.color = "yellow"
+  const shotClock = document.getElementsByClassName('shot-clock')[0];
+  shotClock.textContent = state.shotClock;
+  shotClock.style.color = 'yellow';
   if (state.shotClock <= 5) {
-  shotClock.style.color = "red"
+    shotClock.style.color = 'red';
   }
-}
+};
 
-const renderPlayButton = ( ) => {
-  let playButton = document.getElementsByClassName("play")[0]
+const renderPlayButton = () => {
+  const playButton = document.getElementsByClassName('play')[0];
   const labels = {
-    "Unstarted": "Play",
-    "Playing": "Pause",
-    "Paused": "Resume"
-  }
-  playButton.textContent = labels[state.gameStatus]
-}
+    Unstarted: 'Play',
+    Playing: 'Pause',
+    Paused: 'Resume',
+  };
+  playButton.textContent = labels[state.gameStatus];
+};
 
 const renderScoreButtons = () => {
-  let scoreButtons = document.getElementsByClassName("score-button")
+  const scoreButtons = document.getElementsByClassName('score-button');
   Array.from(scoreButtons).forEach((button) => {
     const buttonLabels = {
-     "home3" : state.homeTeam + " 3 points",
-     "home2" : state.homeTeam + " 2 points",
-     "visitor2": state.visitorTeam + " 2 points",
-     "visitor3": state.visitorTeam + " 3 points"
-    } 
-    button.textContent = buttonLabels[button.classList [1]]
+      home3: `${state.homeTeam} 3 points`,
+      home2: `${state.homeTeam} 2 points`,
+      visitor2: `${state.visitorTeam} 2 points`,
+      visitor3: `${state.visitorTeam} 3 points`,
+    };
+    button.textContent = buttonLabels[button.classList[1]];
   });
-}
+};
 
 const renderTeamNames = () => {
-  let teamNames = document.getElementsByClassName("team-name")
+  const teamNames = document.getElementsByClassName('team-name');
   Array.from(teamNames).forEach((team) => {
     const teamLabels = {
-    "home-team-name": state.homeTeam,
-    "visitor-team-name": state.visitorTeam
-    }
-    team.textContent = teamLabels[team.classList [1]]
+      'home-team-name': state.homeTeam,
+      'visitor-team-name': state.visitorTeam,
+    };
+    team.textContent = teamLabels[team.classList[1]];
   });
-}
+};
 const renderRanks = () => {
-  let ranks = document.getElementsByClassName("rank")
+  const ranks = document.getElementsByClassName('rank');
   Array.from(ranks).forEach((rank) => {
     const rankLabels = {
-    "home-rank": state.homeRank,
-    "visitor-rank": state.visitorRank
-    }
-    rank.textContent = rankLabels[rank.classList [1]]
+      'home-rank': state.homeRank,
+      'visitor-rank': state.visitorRank,
+    };
+    rank.textContent = rankLabels[rank.classList[1]];
   });
-}
+};
